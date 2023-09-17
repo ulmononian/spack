9540f6d1d416106ec4396c15b4763aa1ce9d92cc
ulmononian <cameron_book@alumni.brown.edu>
2023-09-16 23:24:25 -0500

    Add yafyaml to ufs-srw-app-env.

--- var/spack/repos/jcsda-emc-bundles/packages/ufs-srw-app-env/package.py
+++ var/spack/repos/jcsda-emc-bundles/packages/ufs-srw-app-env/package.py
@@ -48,5 +48,6 @@ class UfsSrwAppEnv(BundlePackage):
     depends_on("metplus")
     depends_on("mapl")
     depends_on("ncio")
+    depends_on("yafyaml")
 
     # There is no need for install() since there is no code.
