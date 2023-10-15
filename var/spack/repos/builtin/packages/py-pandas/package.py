4d69864882deb5af0c5cf7804a0c07f9bd0200ca
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-10-14 19:22:12 -0600

    Define excel dependencies for py-pandas 1.4, 1.5, 2.0, 2.1

--- var/spack/repos/builtin/packages/py-pandas/package.py
+++ var/spack/repos/builtin/packages/py-pandas/package.py
@@ -17,7 +17,7 @@ class PyPandas(PythonPackage):
 
     maintainers("adamjstewart")
 
-    variant("excel", default=False, description="Build with support for Excel (py-openpyxl)")
+    variant("excel", when="@1.5:", default=False, description="Build with support for Excel (py-openpyxl)")
 
     version("2.0.3", sha256="c02f372a88e0d17f36d3093a644c73cfc1788e876a7c4bcb4020a77512e2043c")
     version("2.0.2", sha256="dd5476b6c3fe410ee95926873f377b856dbc4e81a9c605a0dc05aaccc6a7c6c6")
@@ -129,9 +129,18 @@ class PyPandas(PythonPackage):
     # Optional dependencies
     # https://pandas.pydata.org/pandas-docs/stable/getting_started/install.html#optional-dependencies
 
-    # https://github.com/spack/spack/issues/40452
-    depends_on("py-openpyxl", type=("run"), when="+excel")
-    depends_on("py-openpyxl@3.0.7:", type=("run"), when="@1.5.3: +excel")
+    # Excel dependencies for 1.4+ (not coded up for earlier versions)
+    depends_on("py-xlrd@2.0.1:", type=("run"), when="@1.4: +excel")
+    depends_on("py-xlwt@1.3.0:", type=("run"), when="@1.4:1.5 +excel")
+    depends_on("py-xlsxwriter@1.2.2:", type=("run"), when="@1.4: +excel")
+    depends_on("py-xlsxwriter@1.4.3:", type=("run"), when="@1.5: +excel")
+    depends_on("py-xlsxwriter@3.0.3:", type=("run"), when="@2.1: +excel")
+    depends_on("py-openpyxl@3.0.3:", type=("run"), when="@1.4: +excel")
+    depends_on("py-openpyxl@3.0.7:", type=("run"), when="@1.5: +excel")
+    depends_on("py-openpyxl@3.0.10:", type=("run"), when="@2.1: +excel")
+    depends_on("py-pyxlsb@1.0.6:", type=("run"), when="@1.4: +excel")
+    depends_on("py-pyxlsb@1.0.8:", type=("run"), when="@1.5: +excel")
+    depends_on("py-pyxlsb@1.0.9:", type=("run"), when="@2.1: +excel")
 
     # Historical dependencies
     depends_on("py-setuptools@61:", when="@2.0", type="build")
