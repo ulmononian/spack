27263653e58ceec75db5e0e02a7f6ff49ab604c1
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-11-21 14:17:58 -0700

    New package py-graphene

---
+++ var/spack/repos/builtin/packages/py-graphene/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyGraphene(PythonPackage):
+    """GraphQL framework for Python"""
+
+    homepage = "https://graphene-python.org/"
+    pypi = "graphene/graphene-2.1.9.tar.gz"
+    maintainers("climbfuji")
+
+    version("2.1.9", sha256="b9f2850e064eebfee9a3ef4a1f8aa0742848d97652173ab44c82cc8a62b9ed93")
+
+    depends_on("py-setuptools", type="build")
+    depends_on("py-graphql-core@2.1:2", type=("build", "run"))
+    depends_on("py-graphql-relay@2:2", type=("build", "run"))
+    depends_on("py-aniso8601@3:7", type=("build", "run"))
+    depends_on("py-six@1.10:1", type=("build", "run"))
