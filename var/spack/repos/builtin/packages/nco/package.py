00aeb446e41ed460919714a34c414d9b19684f2a
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-01-29 20:24:37 -0700

    Apply Intel-OpenMPI patch for nco@5.0.1 for all versions 5+

--- var/spack/repos/builtin/packages/nco/package.py
+++ var/spack/repos/builtin/packages/nco/package.py
@@ -41,7 +41,7 @@ class Nco(AutotoolsPackage):
     patch("NUL-0-NULL.patch", when="@:4.6.7")
 
     # https://github.com/nco/nco/issues/244
-    patch("nco-5_0_1-intel-omp.patch", when="@5.0.1 %intel")
+    patch("nco-5_0_1-intel-omp.patch", when="@5: %intel")
 
     variant("doc", default=False, description="Build/install NCO TexInfo-based documentation")
 
