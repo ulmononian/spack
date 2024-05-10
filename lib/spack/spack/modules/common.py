c30b4fc075ccccc130beb5873c26529d758ed007
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-05-10 13:31:54 -0600

    Fix new syntax in lib/spack/spack/modules/common.py to have case-preserving {name}_ROOT env variables in auto-generated module files

--- lib/spack/spack/modules/common.py
+++ lib/spack/spack/modules/common.py
@@ -86,13 +86,17 @@ _valid_tokens = (
 _FORMAT_STRING_RE = re.compile(r"({[^}]*})")
 
 
-def _format_env_var_name(spec, var_name_fmt):
+def _format_env_var_name(spec, var_name_fmt, uppercase=True):
     """Format the variable name, but uppercase any formatted fields."""
     fmt_parts = _FORMAT_STRING_RE.split(var_name_fmt)
-    return "".join(
-        spec.format(part).upper() if _FORMAT_STRING_RE.match(part) else part for part in fmt_parts
-    )
-
+    if uppercase:
+        return "".join(
+            spec.format(part).upper() if _FORMAT_STRING_RE.match(part) else part for part in fmt_parts
+        )
+    else:
+        return "".join(
+            spec.format(part) if _FORMAT_STRING_RE.match(part) else part for part in fmt_parts
+        )
 
 def _check_tokens_are_valid(format_string, message):
     """Checks that the tokens used in the format string are valid in
@@ -748,24 +752,16 @@ class BaseContext(tengine.Context):
         exclude = self.conf.exclude_env_vars
 
         # We may have tokens to substitute in environment commands
-
-        ### BEGIN CUSTOM CODE FOR SPACK-STACK-DEV ###
-        # Prepare a suitable transformation dictionary for the names
-        # of the environment variables. This means turn the valid
-        # tokens uppercase.
-        # DH 20240410 - reverting this to case-preserving, see
-        # https://github.com/spack/spack/issues/43569
-        transform = {}
-        for token in _valid_tokens:
-            transform[token] = lambda s, string: string
-        ### END CUSTOM CODE FOR SPACK-STACK-DEV ###
-
         for x in env:
             # Ensure all the tokens are valid in this context
             msg = "some tokens cannot be expanded in an environment variable name"
 
             _check_tokens_are_valid(x.name, message=msg)
-            x.name = _format_env_var_name(self.spec, x.name)
+            # DH* 20240510 - preserve case of package name in {name}_ROOT env variables
+            if "_ROOT" in x.name:
+                x.name = _format_env_var_name(self.spec, x.name, uppercase=False)
+            else:
+                x.name = _format_env_var_name(self.spec, x.name)
             if self.modification_needs_formatting(x):
                 try:
                     # Not every command has a value
